2433ff51086d2dfdcaea6ad4fc2dc02329d6c215
IEAD\2312004 <mandy.li@iisigroup.com>
2025-07-30 11:24:10 +0800

    test

--- src/pages/admin/DashBoard.js
+++ src/pages/admin/DashBoard.js
@@ -38,7 +38,7 @@ function Dashboard() {
     <Message />
       <nav className="navbar navbar-expand-lg bg-dark">
         <div className="container-fluid">
-          <p className="text-white mb-0">HEX EATS 後台管理系統</p>
+          <p className="text-white mb-0">TEST HEX EATS 後台管理系統</p>
           <button
             className="navbar-toggler"
             type="button"
