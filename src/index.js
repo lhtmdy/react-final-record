ecb14ac7a5bc8c6842c5a029a831571016679796
IEAD\2312004 <mandy.li@iisigroup.com>
2025-08-14 15:41:00 +0800

    taildwind

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import 'bootstrap/scss/bootstrap.scss';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./stylesheets/all.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+import { purpleColor, blueColor, greenColor, pinkColor, yellowColor, grayColor,orangeColor } from './src/tailwindcss/color.js'
+import fontSize from './src/tailwindcss/fontSize.json'
 module.exports = {
   content: [
      "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+    colors: {
+      purple: {
+        ...purpleColor,
+      },
+      blue: {
+        ...blueColor,
+      },
+      green: {
+        ...greenColor,
+      },
+      pink: {
+        ...pinkColor,
+      },
+      yellow: {
+        ...yellowColor,
+      },
+      gray: {
+        ...grayColor,
+      },
+      orange: {
+        ...orangeColor,
+      },
+      transparent: 'transparent',
+      white: '#ffffff',
+    },
+     fontSize: {
+      ...fontSize,
+    },
   },
   plugins: [],
 }
